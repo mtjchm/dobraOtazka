fa8959dfb1a55651b1c6f9917b24039b1eb7b2b0
Matěj Chamrád <matejcham@gmail.com>
2026-06-02 19:48:27 +0200

    Add $data field to Kontroler.php and extract it into view

--- kontrolery/Kontroler.php
+++ kontrolery/Kontroler.php
@@ -1,10 +1,12 @@
 <?php
 abstract class Kontroler {
     protected $pohled = ""; // název souboru s pohledem (bez přípony .phtml)
+    protected $data = [];
 
     abstract public function zpracuj($parametry);
 
     public function vypisPohled() {
+        extract($this->data);
         require "pohledy/{$this->pohled}.phtml";
     }
     
