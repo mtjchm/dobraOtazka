51c6535163d9dcc2c71ff2f414f258a280a857b4
PrejPavel <kamoscz845@gmail.com>
2026-06-04 18:48:52 +0200

    Balik_vytvor

--- kontrolery/BalikyKontroler.php
+++ kontrolery/BalikyKontroler.php
@@ -4,8 +4,16 @@ class BalikyKontroler extends Kontroler {
         $this->pohled = "baliky";
         $this->data = array();
 
-        //kontroluje jestli jsme na specificke karte nebo ne a podle toho zobrazuje informace
+        // 1. KROK: Pokud jde uživatel na /baliky/vytvor, rovnou mu dej pohled pro vytvoření
+        if (isset($parametry[0]) && $parametry[0] === 'vytvor') {
+            // Vytáhneme VŠECHNY karty z tabulky karty pro deck building
+            $this->data['all_cards'] = Db::dotazVsechny("SELECT name, id, deck, row, strength, ability, filename FROM karty");
+            
+            $this->pohled = "balik_vytvor";
+            return; // Ukončíme metodu, zbytek kódu se nespustí
+        }
 
+        // kontroluje jestli jsme na specificke karte nebo ne a podle toho zobrazuje informace
         if (isset($parametry[0])) {
             $this->data['isDetail'] = true;
             $balikId = $parametry[0];
@@ -27,7 +35,6 @@ class BalikyKontroler extends Kontroler {
             $this->data['deck']  = $balik;
             $this->data['cards'] = $balik->getKarty();
             $this->data['tags']  = $balik->getTagy();
-            
             
         } else {
             $this->data['isDetail'] = false;
