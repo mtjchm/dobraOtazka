ee336fe113c1f73ed647e0be20327e78b86da593
PrejPavel <kamoscz845@gmail.com>
2026-06-04 15:29:06 +0200

    ive put _ before everything so it is now a superglobal variable instead of a local variable which i think is what its meant to be

--- kontrolery/LoginKontroler.php
+++ kontrolery/LoginKontroler.php
@@ -2,28 +2,28 @@
 class LoginKontroler extends Kontroler {
     public function zpracuj($parametry) {
         $this->data = array();
-        if (isset($GET)) {
-            if (isset($SESSION) && isset($SESSION['uid'])) {
+        if (isset($_GET)) {
+            if (isset($_SESSION) && isset($_SESSION['uid'])) {
                 //vykopne uživatele když je přihlášený
                 $this->pohled = "main";
                 return;
             };
 
             $this->pohled = "login";
-        } else if (isset($POST) && !empty($POST['heslo']) && !empty($POST['username'])) {
-            if (isset($SESSION) && isset($SESSION['uid'])) {
+        } else if (isset($_POST) && !empty($_POST['heslo']) && !empty($_POST['username'])) {
+            if (isset($_SESSION) && isset($_SESSION['uid'])) {
                 //odhlásí uživatele
                 session_destroy();
             } else {
-                $uzivatel = Uzivatel::getByUsername($POST["username"]);
-                if(is_null($uzivatel) || !$uzivatel->porovnejHeslo($POST["password"])) {
+                $uzivatel = Uzivatel::getByUsername($_POST["username"]);
+                if(is_null($uzivatel) || !$uzivatel->porovnejHeslo($_POST["password"])) {
                     $this->data["loginOK"] = false;
                     $this->pohled = "login";
                     return;
                 }
 
-                $SESSION['uid'] = $uzivatel->uid;
-                $SESSION['username'] = $uzivatel->username;
+                $_SESSION['uid'] = $uzivatel->uid;
+                $_SESSION['username'] = $uzivatel->username;
 
                 $this->data["loginOK"] = true;
                 $this->pohled = "main";
