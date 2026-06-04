ed86794c97354ac13d1036305521da55a86341de
PrejPavel <kamoscz845@gmail.com>
2026-06-04 15:02:57 +0200

    Decks are now visble on the main screen in the center panel

--- kontrolery/MainKontroler.php
+++ kontrolery/MainKontroler.php
@@ -1,6 +1,16 @@
 <?php
 class MainKontroler extends Kontroler {
-    public function zpracuj($paramentry) {
+    public function zpracuj($parametry) {
         $this->pohled = "main";
+
+        $radky = Db::dotazVsechny("
+            SELECT balicek_id, nazev, popis, autor_balicek
+            FROM balicek
+        ");
+
+        $this->data['decks'] = array_map(
+            fn($radek) => Balik::newFromAssocArray($radek),
+            $radky
+        );
     }
 }
