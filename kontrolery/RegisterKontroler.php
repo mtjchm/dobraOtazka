a0da0533505219e31cf91b753e9fa9f447b68973
Matěj Chamrád <matejcham@gmail.com>
2026-06-11 23:32:54 +0200

    Improvements to register

--- kontrolery/RegisterKontroler.php
+++ kontrolery/RegisterKontroler.php
@@ -12,10 +12,12 @@ class RegisterKontroler extends Kontroler {
             };
 
             $this->pohled = "register";
-        } else if (isset($_SESSION) && isset($_SESSION['uid'])) {
-            //odhlásí uživatele
-            session_destroy();
         } else if (isset($_POST) && !empty($_POST['password']) && !empty($_POST['username'])) {
+            if (mb_strlen((string)$_POST["password"]) < 8 || !preg_match("/.*[0-9].*/", $_POST["password"]) || mb_strlen((string)$_POST["username"]) < 3) {
+                $this->data["invalidError"] = true;
+                $this->pohled = "register";
+                return;
+            }
             $uzivatel = Uzivatel::getByUsername($_POST["username"]);
             if(!is_null($uzivatel)) {
                 $this->data["userExists"] = true;
